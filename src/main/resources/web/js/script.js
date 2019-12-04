706cb8add38d097429b15875db70ea866892e16a
maheswara.reddy <p.mahesh973@gmail.com>
2019-12-04 11:00:28 +0530

    t

--- src/main/resources/web/js/script.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-
-  $('ul.hover_block li').hover(function(){
-    $(this).find('img').animate({top:'182px'},{queue:false,duration:500});
-  }, function(){
-    $(this).find('img').animate({top:'0px'},{queue:false,duration:500});
-  });
-});
